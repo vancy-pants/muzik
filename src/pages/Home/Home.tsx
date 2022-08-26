0aa101a15547768e66bca6763a4eb56e6f0b56b1
Alex Vance <me@alexvance.io>
2022-08-26 10:54:25 -0600

    remove Center component

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,7 +1,6 @@
 import {
   ActionIcon,
   Anchor,
-  Center,
   Group,
   Image,
   Paper,
@@ -69,49 +68,47 @@ function Home() {
         </ActionIcon>
       </Stack>
       <Space h="md" />
-      <Center>
-        <Stack>
-          {albums.map((album) => (
-            <Paper
-              key={album.id.attributes['im:id']}
-              component={Group}
-              shadow="xs"
-              p="md"
-              withBorder
-              className="album-card"
-            >
-              <Image
-                radius="md"
-                src={album['im:image'][2].label}
-                alt={`${album['im:name'].label} album cover`}
-                width={150}
-              />
-              <Stack className="album-info">
-                <Text size="lg" weight={700}>
-                  Album Name: {album['im:name'].label}
-                </Text>
-                <Text>
-                  Artist:{' '}
-                  {album['im:artist'].attributes &&
-                  album['im:artist'].attributes.href ? (
-                    <Anchor
-                      href={album['im:artist'].attributes.href}
-                      target="_blank"
-                      rel="noopener noreferrer"
-                    >
-                      {album['im:artist'].label}
-                    </Anchor>
-                  ) : (
-                    album['im:artist'].label
-                  )}
-                </Text>
-                <Text>Track Count: {album['im:itemCount'].label}</Text>
-                <Text>Price: {album['im:price'].label}</Text>
-              </Stack>
-            </Paper>
-          ))}
-        </Stack>
-      </Center>
+      <Stack align="center">
+        {albums.map((album) => (
+          <Paper
+            key={album.id.attributes['im:id']}
+            component={Group}
+            shadow="xs"
+            p="md"
+            withBorder
+            className="album-card"
+          >
+            <Image
+              radius="md"
+              src={album['im:image'][2].label}
+              alt={`${album['im:name'].label} album cover`}
+              width={150}
+            />
+            <Stack className="album-info">
+              <Text size="lg" weight={700}>
+                Album Name: {album['im:name'].label}
+              </Text>
+              <Text>
+                Artist:{' '}
+                {album['im:artist'].attributes &&
+                album['im:artist'].attributes.href ? (
+                  <Anchor
+                    href={album['im:artist'].attributes.href}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    {album['im:artist'].label}
+                  </Anchor>
+                ) : (
+                  album['im:artist'].label
+                )}
+              </Text>
+              <Text>Track Count: {album['im:itemCount'].label}</Text>
+              <Text>Price: {album['im:price'].label}</Text>
+            </Stack>
+          </Paper>
+        ))}
+      </Stack>
     </Stack>
   )
 }
